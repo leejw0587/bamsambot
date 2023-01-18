3d6156cf80cead618956f5c7923187482dfc509e
leejw0587 <leejw0587@gmail.com>
2023-01-18 10:34:27 +0900

    verify

--- cogs/owner.py
+++ cogs/owner.py
@@ -4,93 +4,31 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
-from helpers import checks, db_manager
+from helpers import checks, db_manager, embeds
+
+
+class VerifyButton(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.Button(
+        label="인증 / Verify",
+        style=discord.ButtonStyle.green,
+        custom_id="verify"
+    )
+    async def verify(self, interaction: discord.Interaction, button: discord.ui.Button):
+        Check_Role = interaction.guild.get_role(390821573315002369)
+
+        if Check_Role not in interaction.user.roles:
+            await interaction.user.add_roles(Check_Role)
+            await interaction.user.send_message(embed=embeds.EmbedGreen("인증", "인증이 완료되었습니다!"))
+        else:
+            await interaction.user.send_message(embed=embeds.EmbedRed("인증", "이미 인증이 완료된 계정입니다!"))
 
 
 class Owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
-
-    # @commands.command(
-    #     name="sync",
-    #     description="Synchonizes the slash commands.",
-    # )
-    # @app_commands.describe(scope="The scope of the sync. Can be `global` or `guild`")
-    # @checks.is_owner()
-    # async def sync(self, context: Context, scope: str) -> None:
-    #     """
-    #     Synchonizes the slash commands.
-
-    #     :param context: The command context.
-    #     :param scope: The scope of the sync. Can be `global` or `guild`.
-    #     """
-
-    #     if scope == "global":
-    #         await context.bot.tree.sync()
-    #         embed = discord.Embed(
-    #             title="Slash Commands Sync",
-    #             description="Slash commands have been globally synchronized.",
-    #             color=0x9C84EF
-    #         )
-    #         await context.send(embed=embed)
-    #         return
-    #     elif scope == "guild":
-    #         context.bot.tree.copy_global_to(guild=context.guild)
-    #         await context.bot.tree.sync(guild=context.guild)
-    #         embed = discord.Embed(
-    #             title="Slash Commands Sync",
-    #             description="Slash commands have been synchronized in this guild.",
-    #             color=0x9C84EF
-    #         )
-    #         await context.send(embed=embed)
-    #         return
-    #     embed = discord.Embed(
-    #         title="Invalid Scope",
-    #         description="The scope must be `global` or `guild`.",
-    #         color=0xE02B2B
-    #     )
-    #     await context.send(embed=embed)
-
-    # @commands.command(
-    #     name="unsync",
-    #     description="Unsynchonizes the slash commands.",
-    # )
-    # @app_commands.describe(scope="The scope of the sync. Can be `global`, `current_guild` or `guild`")
-    # @checks.is_owner()
-    # async def unsync(self, context: Context, scope: str) -> None:
-    #     """
-    #     Unsynchonizes the slash commands.
-
-    #     :param context: The command context.
-    #     :param scope: The scope of the sync. Can be `global`, `current_guild` or `guild`.
-    #     """
-
-    #     if scope == "global":
-    #         context.bot.tree.clear_commands(guild=None)
-    #         await context.bot.tree.sync()
-    #         embed = discord.Embed(
-    #             title="Slash Commands Unsync",
-    #             description="Slash commands have been globally unsynchronized.",
-    #             color=0x9C84EF
-    #         )
-    #         await context.send(embed=embed)
-    #         return
-    #     elif scope == "guild":
-    #         context.bot.tree.clear_commands(guild=context.guild)
-    #         await context.bot.tree.sync(guild=context.guild)
-    #         embed = discord.Embed(
-    #             title="Slash Commands Unsync",
-    #             description="Slash commands have been unsynchronized in this guild.",
-    #             color=0x9C84EF
-    #         )
-    #         await context.send(embed=embed)
-    #         return
-    #     embed = discord.Embed(
-    #         title="Invalid Scope",
-    #         description="The scope must be `global` or `guild`.",
-    #         color=0xE02B2B
-    #     )
-    #     await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="load",
@@ -336,6 +274,15 @@ class Owner(commands.Cog, name="owner"):
     #                 json.dump(userdata, file, indent="\t", ensure_ascii=False)
 
     #     await context.send("Initialized `userdata.json`")
+
+    @commands.hybrid_command(
+        name="verification",
+        description="인증 embed를 만듭니다. (창조자 전용)"
+    )
+    @checks.is_owner()
+    async def verification(self, context: Context):
+        context.send(embed=embeds.EmbedBlurple(
+            "인증", "아래 버튼을 눌러 인증을 진행해주세요."), view=VerifyButton())
 
 
 async def setup(bot):
