31eb60e0b7430e720c1f15a7fcc29973a41a14ee
leejw0587 <leejw0587@gmail.com>
2023-02-03 18:08:00 +0900

    Update

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-**[ To-Do ]**
+**[ BAMSAMBOT ]**
+디스코드 커뮤니티 뱀샘크루를 위한 자체 개발 디스코드 봇  
+타 서버 사용 불가
 
-- 통화방 커스텀 기능
-- 레벨링 시스템
-- Helpers-Colors.py-Class color
-- Leaderboard 시스템 (레벨, 페리도트)
+초대 링크: https://discord.com/oauth2/authorize?&client_id=1070945112168861786&scope=bot+applications.commands&permissions=8
+https://discord.com/oauth2/authorize?&client_id=1070945761480679475&scope=bot+applications.commands&permissions=8
 
-<Leveling System>
-/level set 
-/xp set
+1070945112168861786
--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ async def on_message(message: discord.Message) -> None:
 
 @bot.event
 async def on_member_join(member):
-    welcome_channel = bot.get_channel(544913480252260382)
+    welcome_channel = bot.get_channel(1070686070011871272)
 
     embed = discord.Embed(
         title="WELCOME", description=f"{member.mention}님, 뱀샘크루에 오신 것을 환영합니다!\n<#412231022533410817> 한 번 확인해주세요!", color=0x9C84EF)
@@ -113,7 +113,7 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
-    welcome_channel = bot.get_channel(544913480252260382)
+    welcome_channel = bot.get_channel(1070686070011871272)
 
     embed = discord.Embed(
         title="GOOD BYE", description=f"{member.mention}님이 뱀샘크루를 떠났습니다.", color=0x9C84EF)
@@ -225,4 +225,4 @@ async def load_cogs() -> None:
 
 asyncio.run(init_db())
 asyncio.run(load_cogs())
-bot.run(config["main_token"])
+bot.run(config["test_token"])
