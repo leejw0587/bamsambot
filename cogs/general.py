957f2e617add868aedf13f849fbc408526b1239d
leejw0587 <leejw0587@gmail.com>
2023-02-07 21:28:42 +0900

    Add defer

--- cogs/general.py
+++ cogs/general.py
@@ -466,6 +466,7 @@ class General(commands.Cog, name="general"):
     )
     async def code_redeem(self, context: Context):
         await context.interaction.response.send_modal(RedeemModal())
+        await context.defer()
 
     @code.command(
         name='create',
